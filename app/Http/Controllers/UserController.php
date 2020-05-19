573cc20159edc45356e5f8d0e1cc256fe0a48d6f
Kris <31840452+krisnieh@users.noreply.github.com>
2020-05-19 11:10:33 +0800

    init
    init

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,19 +8,7 @@ class UserController extends Controller
 {
     public function index(Request $request)
     {
-        $app = app('wechat.official_account');
-        // $app['request'] = $request;
-        $app['request'] = $request;
-        $user = $app->oauth->user();
-
-        // 获取 OAuth 授权结果用户信息
-        // $user = $oauth->user();
-        // $user = $app->oauth->user();
-        // $response = $app->oauth->scopes(['snsapi_userinfo']);
-        // $app['request'] = $request;
-
-        // print_r($response->user());
-        print_r($user);
+        return "many many";
     }
 
 
--- app/Http/Middleware/MixAuth.php
+++ app/Http/Middleware/MixAuth.php
@@ -16,12 +16,16 @@ class MixAuth
      */
     public function handle($request, Closure $next)
     {
-        // if(Auth::check()) return $next($request);
+        if(Auth::check()) return $next($request);
 
-        $app = app('wechat.official_account');
+        $user = session('wechat.oauth_user.default');
+
+        print_r($user);
+
+        // $app = app('wechat.official_account');
+        // // $app['request'] = $request;
         // $app['request'] = $request;
-        $app['request'] = $request;
-        $user = $app->oauth->user();
+        // $user = $app->oauth->user();
 
         // 获取 OAuth 授权结果用户信息
         // $user = $oauth->user();
@@ -30,7 +34,8 @@ class MixAuth
         // $app['request'] = $request;
 
         // print_r($response->user());
-        print_r($user);
+        // $user = session('wechat.oauth_user.default'); // 拿到授权用户资料
+        // print_r($user);
 
 
         // return app(\Overtrue\LaravelWeChat\Middleware\OAuthAuthenticate::class)->handle($request, function ($request) use ($next) {
