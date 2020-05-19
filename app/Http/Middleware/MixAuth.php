cfa9a70eb19b2d2c0b84d6c35fbef5b49048f0ed
Kris <31840452+krisnieh@users.noreply.github.com>
2020-05-19 09:09:55 +0800

    init
    init

--- app/Http/Middleware/MixAuth.php
+++ app/Http/Middleware/MixAuth.php
@@ -18,11 +18,21 @@ class MixAuth
     {
         if(Auth::check()) return $next($request);
 
+        $app = app('wechat.official_account');
+        $oauth = $app->oauth;
 
-        return app(\Overtrue\LaravelWeChat\Middleware\OAuthAuthenticate::class)->handle($request, function ($request) use ($next) {
-            $wechat_user = session('wechat.oauth_user.default');
-            print_r($wechat_user);
-        });
+        // 获取 OAuth 授权结果用户信息
+        $user = $oauth->user();
+
+        print_r($user);
+
+
+        // return app(\Overtrue\LaravelWeChat\Middleware\OAuthAuthenticate::class)->handle($request, function ($request) use ($next) {
+        //     $wechat_user = session('wechat.oauth_user.default');
+        //     print_r($wechat_user);
+        // });
+
+        // return app(middleware())
 
 
     }
