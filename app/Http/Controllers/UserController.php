f4ca561803a531a7319821cda38297dd9d94c958
Kris <31840452+krisnieh@users.noreply.github.com>
2020-05-19 10:37:13 +0800

    init
    init

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,13 +6,14 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
         $app = app('wechat.official_account');
         // $oauth = $app->oauth->scopes(['snsapi_userinfo']);
 
         // 获取 OAuth 授权结果用户信息
         // $user = $oauth->user();
+        $app['request'] = $request;
         $user = $app->oauth->user();
 
         print_r($user);
