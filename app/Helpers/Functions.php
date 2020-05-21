d750a37623c34ab2f015347af9f8fc21c99afbbe
Kris <31840452+krisnieh@users.noreply.github.com>
2020-05-22 07:30:59 +0800

    init
    init

--- app/Helpers/Functions.php
+++ app/Helpers/Functions.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Arr;
 
-function show_json($json, $key_chain, $bakcup=false)
+function show($json, $key_chain, $bakcup=false)
 {
     $array = json_decode($json,true);
     return Arr::has($array, $key_chain) ? Arr::get($array, $key_chain) : $bakcup;
