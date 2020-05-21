2b37622349978d54bdf9e130abeff8fe41057ed3
Kris <31840452+krisnieh@users.noreply.github.com>
2020-05-21 17:40:49 +0800

    init
    init

--- routes/web.php
+++ routes/web.php
@@ -52,8 +52,9 @@ Route::get('/test', function () {
 
 Route::get('/expos', function () {
    $a = json_decode(Auth::user()->ids);
+   print_r($a);
 
-   echo $a->wechat->headimgurl;
+//    echo $a->wechat->headimgurl;
     // $a =  User::all()->toArray();
     // // echo $a;
     // print_r($a);
