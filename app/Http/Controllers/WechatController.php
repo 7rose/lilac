5adfb466e0d4e51278a4b2b576efb3b941fcb31e
Kris <31840452+krisnieh@users.noreply.github.com>
2020-05-24 14:09:05 +0800

    init
    init

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -16,9 +16,38 @@ class WechatController extends Controller
     public function serve()
     {
         $app = app('wechat.official_account');
-        $app->server->push(function($message){
-            return "海上牧云欢迎您!";
+        $app->server->push(function ($message) {
+            switch ($message['MsgType']) {
+                case 'event':
+                    return '收到事件消息';
+                    break;
+                case 'text':
+                    return '收到文字消息';
+                    break;
+                case 'image':
+                    return '收到图片消息';
+                    break;
+                case 'voice':
+                    return '收到语音消息';
+                    break;
+                case 'video':
+                    return '收到视频消息';
+                    break;
+                case 'location':
+                    return '收到坐标消息';
+                    break;
+                case 'link':
+                    return '收到链接消息';
+                    break;
+                case 'file':
+                    return '收到文件消息';
+                // ... 其它消息
+                default:
+                    return '海上牧云欢迎您!';
+                    break;
+            }
         });
+
 
         return $app->server->serve();
     }
@@ -32,7 +61,7 @@ class WechatController extends Controller
     public function ad()
     {
         $app = app('wechat.official_account');
-        $reasult = $app->qrcode->temporary('ad_'.Auth::id(), 3000);
+        $reasult = $app->qrcode->temporary('ad_'.Auth::id(), 300);
         $url = $reasult['url'];
 
         return view('ad',compact('url'));
