65bdb13de2111d2fdf75b3b0a66b88863e1c2a98
Kris <31840452+krisnieh@users.noreply.github.com>
2020-05-24 21:41:35 +0800

    init

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+@extends('../nav')
+
+@section('main')
+<div class="nav-pad"></div>
+<div class="column col-12">
+    <div class="empty">
+      <div class="empty-icon"><i class="icon icon-3x icon-mail"></i></div>
+      <p class="empty-title h5">welcome</p>
+      <p class="empty-subtitle">mooibay.com</p>
+      <div class="empty-action">
+        <button class="btn btn-primary">Expo 2020</button>
+      </div>
+    </div>
+</div>
+@endsection
