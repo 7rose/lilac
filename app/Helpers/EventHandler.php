020384fb3c1345ec5f85a10d58c41d0d36323ddc
Kris <31840452+krisnieh@users.noreply.github.com>
2020-06-04 07:50:12 +0800

    Update EventHandler.php

--- app/Helpers/EventHandler.php
+++ app/Helpers/EventHandler.php
@@ -6,15 +6,7 @@ use Illuminate\Support\Facades\Log;
 
 class EventHandler
 {
-    private $message;
 
-    public function __construct($message)
-    {
-        $this->message = $message;
-    }
-    /*
-     * 事件响应函数
-     */
     public function eventHandler()
     {
         // return new Text(['content' => "指令已收到！"]);
