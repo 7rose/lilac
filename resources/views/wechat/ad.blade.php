06eb2ca65bbead16de6cc2857d475d088dd680ea
Kris <31840452+krisnieh@users.noreply.github.com>
2020-06-03 19:33:41 +0800

    init

--- resources/views/wechat/ad.blade.php
+++ resources/views/wechat/ad.blade.php
@@ -14,13 +14,13 @@
         </div>
         <nav class="panel-nav">
             <ul class="tab tab-block" data-tabs="main_tab">
-                @can('customer-qrcode')
+                @can('customer-qrcode',$user)
                 <li class="tab-item active"><a href="#">客户</a></li>
                 @endcan
-                @can('supplier-qrcode')
+                @can('supplier-qrcode',$user)
                 <li class="tab-item"><a href="#">商户</a></li>
                 @endcan
-                @can('partner-qrcode')
+                @can('partner-qrcode',$user)
                 <li class="tab-item"><a href="#">合作</a></li>
                 @endcan
             </ul>
@@ -28,13 +28,13 @@
         <div class="panel-body">
             <ul data-tabs-content="main_tab">
 
-                @can('customer-qrcode')
+                @can('customer-qrcode',$user)
                 <li>Lorem ipsum dolor sit amet.</li>
                 @endcan
-                @can('supplier-qrcode')
+                @can('supplier-qrcode',$user)
                 <li>Lorem ipsum dolor 2</li>
                 @endcan
-                @can('partner-qrcode')
+                @can('partner-qrcode',$user)
                 <li>Lorem ipsum 3</li>
                 @endcan
             </ul>
