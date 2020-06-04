2e337e6935d7412669d4bbbe02c3ce47312d2fb1
Kris <31840452+krisnieh@users.noreply.github.com>
2020-06-04 08:26:41 +0800

    init
    init

--- app/Helpers/EventHandler.php
+++ app/Helpers/EventHandler.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Log;
 class EventHandler
 {
 
-    public function eventHandler()
+    public function handle($payload = null)
     {
         // return new Text(['content' => "指令已收到！"]);
         Log::info($this->message);
