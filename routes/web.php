57b7dcc4bb7ec0f7f951ab1fbcf88e1cc105a931
Kris <31840452+krisnieh@users.noreply.github.com>
2020-06-06 16:02:27 +0800

    init
    init

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -98,10 +99,12 @@ Route::get('/test', function () {
 
 Route::get('/in', function () {
     // $user = App\User::find(5);
-    $user = App\User::find(6);
-    // $user = App\User::find(1);
-    // $user = App\User::find(8);
-    Auth::login($user);
-    print_r($user->info);
+    // $user = App\User::find(6);
+    // // $user = App\User::find(1);
+    // // $user = App\User::find(8);
+    // Auth::login($user);
+    // print_r($user->info);
+
+    echo '<br>'.Redis::get('17821621090');
 });
 
