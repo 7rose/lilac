d371c861453a33705279c552a881863e088a6a52
Kris <31840452+krisnieh@users.noreply.github.com>
2020-06-08 13:51:53 +0800

    Update pay.blade.php

--- resources/views/pay.blade.php
+++ resources/views/pay.blade.php
@@ -4,8 +4,8 @@
 <div class="column col-12">
     <div class="empty">
         <div class="nav-pad"></div>
-      <div class="empty-icon"><h1><i class="fa fa-ban" aria-hidden="true"></i></h1></div>
-      <p class="empty-subtitle">需要授权, 若需继续请联系管理员</p>
+      <div class="empty-icon"><h1><i class="fa fa-jpy" aria-hidden="true"></i></h1></div>
+      <p class="empty-subtitle">您正在购买</p>
       <div class="empty-action">
         <a href="/apps" class="btn btn-primary">&nbsp;&nbsp;应用中心&nbsp;&nbsp;</a>
       </div>
