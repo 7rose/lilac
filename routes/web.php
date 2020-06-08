bca1d12a1f8f4bba73296b46145ea5c5723dd2a9
Kris <31840452+krisnieh@users.noreply.github.com>
2020-06-08 15:38:30 +0800

    init
    init

--- routes/web.php
+++ routes/web.php
@@ -82,7 +82,7 @@ Route::group(['middleware' => ['web', 'wechat.oauth']], function () {
         Route::post('/expos/store', 'ExpoController@store');
 
         // 票
-        Route::get('/pay/expo/{id}', 'TicketController@order');
+        Route::get('/pay/{id}', 'TicketController@order');
     });
 
 });
