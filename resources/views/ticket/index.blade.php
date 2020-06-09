a31315171ab4e0073a621cc549f308a5d65101cf
Kris <31840452+krisnieh@users.noreply.github.com>
2020-06-09 18:17:11 +0800

    Update index.blade.php

--- resources/views/ticket/index.blade.php
+++ resources/views/ticket/index.blade.php
@@ -5,18 +5,17 @@
 
 <div class="container">
     <div class="container col-4 col-md-6 col-sm-10 col-xs-12 p-centered">
-    <a href="/expos/create" class="btn btn-secondary btn-block"><i class="fa fa-magic" aria-hidden="true"></i> 发布</a>
     <p></p>
     @if (isset($tickets) && count($tickets))
-        @foreach ($tickets as $e)
+        @foreach ($tickets as $t)
         <div class="tile tile-centered">
             <div class="tile-content">
-            <div class="tile-title">title</div>
-            <small class="tile-subtitle text-gray">good</small>
-            <small class="tile-subtitle"><i class="fa fa-map-marker" aria-hidden="true"></i> add</small>
+            <div class="tile-title">{{ show($t->expo->info, 'title') }}</div>
+            <small class="tile-subtitle text-gray">{{ face($t->user->name) }}{{ show($t->user->info, 'mobile.number') }}</small>
+            <small class="tile-subtitle"><i class="fa fa-map-marker" aria-hidden="true"></i> {{ $t->expo->begin }}</small>
             </div>
             <div class="tile-action">
-            <a class="btn btn-link" href="/ticket/{{ $e->id }}">
+            <a class="btn btn-link" href="/ticket/{{ $t->id }}">
                 <h5><i class="fa fa-qrcode" aria-hidden="true"></i></h5>
             </a>
             </div>
