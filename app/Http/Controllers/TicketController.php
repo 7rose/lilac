aa9c5ba9403949491de871c3bbce49605dece673
Kris <31840452+krisnieh@users.noreply.github.com>
2020-06-10 13:16:36 +0800

    init

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -140,10 +140,10 @@ class TicketController extends Controller
 
         $url = false;
 
-        // if((!$ticket->used || ($ticket->used && $ticket->akf)) && $ticket->expo->end > now()){
+        if((!$ticket->used || ($ticket->used && $ticket->akf)) && $ticket->expo->end > now()){
             $qrcode = $this->app->qrcode->temporary('t_'.Auth::id().'_'.$id, 60); # 1分钟
             $url = $qrcode['url'];
-        // }
+        }
 
         return view('ticket.show', compact('ticket', 'url'));
 
