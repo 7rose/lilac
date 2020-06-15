624d7bbe5e451af934f51d1223353c98911b8842
Kris <31840452+krisnieh@users.noreply.github.com>
2020-06-15 11:18:12 +0800

    Update index.blade.php

--- resources/views/ticket/index.blade.php
+++ resources/views/ticket/index.blade.php
@@ -14,7 +14,7 @@
                 <div class="tile-title">{{ show($t->expo->info, 'title') }}&nbsp;&nbsp; ¥ {{ show($t->expo->info, 'price') }}</div>
                 <small class="tile-subtitle text-gray"><i class="fa fa-mobile" aria-hidden="true"></i> {{ show($t->user->ids, 'mobile.number') }} / No.{{ $t->id }}</small><br>
                 <small class="tile-subtitle"><i class="fa fa-clock-o" aria-hidden="true"></i> {{ $t->expo->begin }} <br> {{ $t->expo->end }}</small>
-                <img src="{{ asset('images/line.svg') }}" alt="">
+                <img src="{{ asset('images/line.svg') }}" alt="" class="ticket-line">
                 </div>
                 <div class="tile-action">
                     <a class="btn btn-link" href="/ticket/{{ $t->id }}">
