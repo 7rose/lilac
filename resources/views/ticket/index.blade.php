5abac64f8801db17806ae54ce2d4ef8e4ba45b55
Kris <31840452+krisnieh@users.noreply.github.com>
2020-06-15 10:47:19 +0800

    init
    init

--- resources/views/ticket/index.blade.php
+++ resources/views/ticket/index.blade.php
@@ -14,7 +14,7 @@
             <small class="tile-subtitle text-gray"><i class="fa fa-mobile" aria-hidden="true"></i> {{ show($t->user->ids, 'mobile.number') }} / No.{{ $t->id }}</small><br>
             <small class="tile-subtitle"><i class="fa fa-clock-o" aria-hidden="true"></i> {{ $t->expo->begin }} - {{ $t->expo->end }}</small>
             </div>
-            <img src="{{ asset('images/line.svg') }}" alt="">
+            <img src="{{ asset('images/line.svg') }}" alt="" class="ticket-line">
             <div class="tile-action">
                 <a class="btn btn-link" href="/ticket/{{ $t->id }}">
                     <h5><i class="fa fa-qrcode" aria-hidden="true"></i></h5>
