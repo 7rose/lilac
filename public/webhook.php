7edd0b81d17d073f57e7866fdc05f8f6a453a805
Kris <31840452+krisnieh@users.noreply.github.com>
2020-07-09 07:22:16 +0800

    Update webhook.php

--- public/webhook.php
+++ public/webhook.php
@@ -1,6 +1,6 @@
 <?php
 
-$path = "/var/www/wechat.mooibay.com";
+$path = "/usr/share/caddy/wechat.mooibay.com";
 $key = 'kingking';
 
 $github_signature = @$_SERVER['HTTP_X_HUB_SIGNATURE'];
@@ -14,11 +14,11 @@ $payload_hash = hash_hmac($algo, $payload, $key);
 if($payload_hash == $signature){
     shell_exec('cd '.$path);
     shell_exec('/usr/bin/git pull');
-    shell_exec('/usr/bin/composer install --optimize-autoloader');
+    shell_exec('/usr/bin/composer install');
     shell_exec('/usr/bin/composer dump-autoload');
     shell_exec('/usr/bin/php artisan config:cache');
     // shell_exec('/usr/bin/php artisan route:cache');
-    shell_exec('chown -R nginx:nginx'.$path);
+    // shell_exec('chown -R nginx:nginx'.$path);
     return 200;
 }else{
    return 'invalid key!';
