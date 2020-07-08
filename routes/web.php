b3d211a44fa4a9ab965f4a98d71e3c828fc41cef
Kris <31840452+krisnieh@users.noreply.github.com>
2020-07-09 07:40:48 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -104,7 +104,8 @@ Route::group(['middleware' => ['web', 'wechat.oauth']], function () {
 Route::get('/fake', 'WechatController@fake');
 
 Route::get('/test', function () {
-    echo str_random(16);
+    echo "fuck";
+    // echo str_random(16);
     // $result = '17821621090';
     // $user = App\User::firstOrCreate([
     //     'ids->mobile->number' => $result,
