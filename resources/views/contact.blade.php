9960a6db4f80d56e7b47b9f4a01e6f74e9c49a8a
Kris <31840452+krisnieh@users.noreply.github.com>
2020-07-08 17:41:35 +0800

    Update contact.blade.php

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,7 +4,8 @@
 <div class="column col-12 p-centered">
     <div class="empty">
     <div class="nav-pad"></div>
-      联系方式
+      上海牧云玩具设计有限公司
+      july@mooitoys.com
       <div class="nav-pad"></div>
     </div>
 </div>
