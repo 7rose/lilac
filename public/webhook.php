265eda4baf1246565a2ff84824792339b8f33f29
Kris <31840452+krisnieh@users.noreply.github.com>
2020-07-09 07:37:48 +0800

    Update webhook.php

--- public/webhook.php
+++ public/webhook.php
@@ -1,6 +1,6 @@
 <?php
 
-$path = "/usr/share/caddy/wechat.mooibay.com";
+// $path = "/usr/share/caddy/wechat.mooibay.com";
 $key = 'kingking';
 
 $github_signature = @$_SERVER['HTTP_X_HUB_SIGNATURE'];
@@ -12,13 +12,14 @@ $signature = $arr[1];
 
 $payload_hash = hash_hmac($algo, $payload, $key);
 if($payload_hash == $signature){
-    shell_exec('cd '.$path);
-    shell_exec('/usr/bin/git pull');
-    shell_exec('/usr/bin/composer install');
-    shell_exec('/usr/bin/composer dump-autoload');
-    shell_exec('/usr/bin/php artisan config:cache');
-    // shell_exec('/usr/bin/php artisan route:cache');
-    // shell_exec('chown -R nginx:nginx'.$path);
+    exec('deploy/git.sh');
+    // shell_exec('cd '.$path);
+    // shell_exec('/usr/bin/git pull');
+    // shell_exec('/usr/bin/composer install');
+    // shell_exec('/usr/bin/composer dump-autoload');
+    // shell_exec('/usr/bin/php artisan config:cache');
+    // // shell_exec('/usr/bin/php artisan route:cache');
+    // // shell_exec('chown -R nginx:nginx'.$path);
     return 200;
 }else{
    return 'invalid key!';
