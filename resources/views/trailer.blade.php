ff9ad1da4e3b5e36ba713fe14f363f1ff0f7cb2e
Kris <31840452+krisnieh@users.noreply.github.com>
2020-07-10 08:04:50 +0800

    init

--- resources/views/trailer.blade.php
+++ resources/views/trailer.blade.php
@@ -12,9 +12,10 @@
                 <div class="card-header">
                     <h5>SSF  2020 夏季展</h5>
                     <p><i class="fa fa-clock-o" aria-hidden="true"></i> 日期: 2020/7/25 至 7/26 <br>
-                    <i class="fa fa-map-marker" aria-hidden="true"></i> 地点: 上海市静安区万航渡路838号柒彩里5层</p>
-                    本次盛会由 First Meet Gallery x Mooi Design 携手打造,为您呈现无限精彩!
+                        <i class="fa fa-map-marker" aria-hidden="true"></i> 地点: 上海市静安区万航渡路838号柒彩里5层</p>
+                        本次盛会由 First Meet Gallery x Mooi Design 携手打造,为您呈现无限精彩!
                 </div>
+                    
                 <div class="card-body">
                     <small>
                         “乘风破浪”的娃友们，重拾起自己沉寂已久的玩心，来遇见让你再次心动不已的玩具吧。在SSF面基群内老友，吐露这段时间的心情；结识新友，创造新的美好回忆；还可以亲口把自己的支持告诉你喜欢的设计师，更重要的是，pick自己喜爱的玩具带回家！
@@ -30,9 +31,14 @@
                 </div>
             </div>
         </div>
-
+        <p></p>
+        <div class="card container">
+            <div class="card-image"><img class="img-responsive" src="{{ asset('images/find.jpg') }}" alt=".."></div>
+        </div>
     </div>
-    <div class="card-image"><img class="img-responsive" src="{{ asset('images/bjbs.svg') }}" alt=".."></div>
+    <p></p>
+    <p></p>
+    <div class="card-image"><img class="img-responsive" src="{{ asset('images/bjbs.jpg') }}" alt=".."></div>
         
 </div>
 
