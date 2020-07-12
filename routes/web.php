ef7d79729868f24bff8f0aefec02c2c25d53ac78
Kris <31840452+krisnieh@users.noreply.github.com>
2020-07-12 22:13:45 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -101,6 +101,7 @@ Route::group(['middleware' => ['web', 'wechat.oauth']], function () {
 Route::get('/fake', 'WechatController@fake');
 
 Route::get('/test', function () {
+    abort('403');
     // $users = App\User::has('tickets')->get()->count();
     // // $u1 =  App\User::where(function($query))->get()->count();
     // $u1 = App\User::has('tickets',1)->get()->count();
