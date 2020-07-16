d1aef1c1e8fae98951acfd2bb8f2ffe59493d068
Kris <31840452+krisnieh@users.noreply.github.com>
2020-07-16 12:36:34 +0800

    init

--- app/Exports/Ticket25Export.php
+++ app/Exports/Ticket25Export.php
@@ -3,9 +3,10 @@
 namespace App\Exports;
 
 use App\Ticket;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\FromCollection;
 
-class Ticket25Export implements FromCollection
+class Ticket25Export implements FromCollection, WithHeadings
 {
     /**
     * @return \Illuminate\Support\Collection
@@ -13,5 +14,13 @@ class Ticket25Export implements FromCollection
     public function collection()
     {
         return Ticket::select('id', 'created_at')->where('expo_id', 1)->orderBy('id')->get();
+    }
+
+    public function headings(): array
+    {
+        return[
+            '票号',
+            '日期',                      
+        ];
     }
 }
--- app/Exports/Ticket26Export.php
+++ app/Exports/Ticket26Export.php
@@ -3,9 +3,10 @@
 namespace App\Exports;
 
 use App\Ticket;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\FromCollection;
 
-class Ticket26Export implements FromCollection
+class Ticket26Export implements FromCollection, WithHeadings
 {
     /**
     * @return \Illuminate\Support\Collection
@@ -13,5 +14,13 @@ class Ticket26Export implements FromCollection
     public function collection()
     {
         return Ticket::select('id', 'created_at')->where('expo_id', 2)->orderBy('id')->get();
+    }
+
+    public function headings(): array
+    {
+        return[
+            '票号',
+            '日期',                      
+        ];
     }
 }
