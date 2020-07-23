036032325c3e9800b7b9713b5eccb70a6dd04da1
Kris <31840452+krisnieh@users.noreply.github.com>
2020-07-23 13:50:59 +0800

    Update TicketController.php

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -53,7 +53,7 @@ class TicketController extends Controller
 
         $info = [
             'body'         => show($expo->info, 'title', '') . '电子门票',
-            // 'out_trade_no' => $out_trade_no,
+            'out_trade_no' => $out_trade_no,
             'total_fee'    => show($expo->info, 'price') ? floor(floatval(show($expo->info, 'price')) * 100) : 20000,   // 分
             // 'spbill_create_ip' => '123.12.12.123',                                                                             // 可选，如不传该参数，SDK 将会自动获取相应 IP 地址
             // 'notify_url'       => 'https://pay.weixin.qq.com/wxpay/pay.action',                                                // 支付结果通知网址，如果不设置则会使用配置里的默认地址
@@ -70,10 +70,12 @@ class TicketController extends Controller
 
             // 写入
             // Order::create($info);
-            $resault = Order::firstOrCreate(
-                ['out_trade_no' => $out_trade_no],
-                $info,
-            );
+            $ex = Order::where('out_trade_no', $out_trade_no)->first();
+            if(!$ex) Order::create($info);
+            // $resault = Order::firstOrCreate(
+            //     ['out_trade_no' => $out_trade_no],
+            //     $info,
+            // );
 
         } else {
             abort('510');
