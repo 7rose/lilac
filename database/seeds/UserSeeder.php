40d265abe7122244f67c5ce3eec055ec20ab9c94
Kris <31840452+krisnieh@users.noreply.github.com>
2020-08-16 22:51:39 +0800

    Update UserSeeder.php

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -15,43 +15,43 @@ class UserSeeder extends Seeder
 
         User::create([
             'ids'  => ['mobile' => ['number' => '13761058983', 'active' => true]],
-            'info' => ['name' => '鲁娴婷', 'nick' => 'fanny'],
+            'info' => ['name' => '鲁婷', 'nick' => 'fanny'],
             'conf' => ['roles' => [['org_id' => 8, 'role_id' => 19], ['org_id' => 10, 'role_id' => 25, 'default' => true]]],
         ]);
 
         User::create([
             'ids'  => ['mobile' => ['number' => '18701782407', 'active' => true], 'email' => ['addr' => 'july@mooibay.com', 'active' => true]],
-            'info' => ['name' => '吕洁', 'nick' => 'july'],
+            'info' => ['name' => '吕洁洁', 'nick' => 'july'],
             'conf' => ['roles' => [['org_id' => 4, 'role_id' => 4, 'default' => true], ['org_id' => 10, 'role_id' => 26]]],
         ]);
 
         User::create([
             'ids'  => ['mobile' => ['number' => '13482628335', 'active' => true]],
-            'info' => ['name' => '姚远', 'nick' => 'golden'],
+            'info' => ['name' => '远先生', 'nick' => 'golden'],
             'conf' => ['roles' => [['org_id' => 6, 'role_id' => 9, 'default' => true], ['org_id' => 10, 'role_id' => 26]]],
         ]);
 
         User::create([
             'ids'  => ['mobile' => ['number' => '15911599729', 'active' => true]],
-            'info' => ['name' => '蒋海明', 'nick' => 'joe'],
+            'info' => ['name' => '蒋海', 'nick' => 'joe'],
             'conf' => ['roles' => [['org_id' => 10, 'role_id' => 26]]],
         ]);
 
         User::create([
             'ids'  => ['mobile' => ['number' => '13901752021', 'active' => true]],
-            'info' => ['name' => '倪昌盛', 'nick' => 'kris'],
+            'info' => ['name' => '倪先生', 'nick' => 'kris'],
             'conf' => ['roles' => [['org_id' => 1, 'role_id' => 1], ['org_id' => 5, 'role_id' => 7, 'default' => true], ['org_id' => 10, 'role_id' => 26]]],
         ]);
 
         User::create([
             'ids'  => ['mobile' => ['number' => '19951510566', 'active' => true]],
-            'info' => ['name' => '戴玉', 'nick' => 'frola'],
+            'info' => ['name' => '姚倩', 'nick' => 'frola'],
             'conf' => ['roles' => [['org_id' => 1, 'role_id' => 2], ['org_id' => 6, 'role_id' => 13, 'default' => true]]],
         ]);
 
         User::create([
             'ids'  => ['mobile' => ['number' => '13951195320', 'active' => true]],
-            'info' => ['name' => '仲一玲', 'nick' => 'bella'],
+            'info' => ['name' => '仲菲菲', 'nick' => 'bella'],
             'conf' => ['roles' => [['org_id' => 6, 'role_id' => 15]]],
         ]);
 
